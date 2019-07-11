8ee974894856f50a3957a58203f5b11f9f2befcc
Tester <ittruemrdro4@yandex.ru>
2019-07-11 16:28:27 +0500

    lenta

--- backend/config/index.js
+++ backend/config/index.js
@@ -20,26 +20,3 @@ module.exports = {
   emailUser: 'ittruemrdro4@yandex.ru',
   emailPassword: 'RbH.IrFwBcFhMhEkBn123456789RbH.IrFrEk123456789',
 }
-
-// module.exports = {
-//   port: 3000,
-//   portSocket: 3001,
-//   secret: 'fitlancesecret123456789',
-//   session: { session: false },
-//   dbProd: {
-//     host: "startzak.mysql.tools",
-//     port: 3306,
-//     dbuser: "startzak_fitfit",
-//     dbpass: ")97p8%ylZI",
-//     dbname: "startzak_fitfit"
-//   },
-//   dbDev: {
-//     host: "startzak.mysql.tools",
-//     port: 3306,
-//     dbuser: "startzak_fitfit",
-//     dbpass: ")97p8%ylZI",
-//     dbname: "startzak_fitfit"
-//   },
-//   emailUser: 'ittruemrdro4@yandex.ru',
-//   emailPassword: 'RbH.IrFwBcFhMhEkBn123456789RbH.IrFrEk123456789',
-// }
